74b13c82b6cbbf6fa756ea498ed53d7cb94ea1d3
fonzroevykjunsay <60122963+fonzroevykjunsay@users.noreply.github.com>
2020-11-21 14:25:57 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,34 @@
+<html>
+<head>
+<link href="https://fonts.googleapis.com/css?family=Nanum+Gothic|Raleway&display=swap" rel="stylesheet">
+<link href="css/style1.css" rel="stylesheet"type="text/css">
+<meta charset="utf-8>
+<title>"Junsay"</title>
+</head>
+
+<body>
+  <header>
+  <div class = "container">
+  <ul>
+        <li>HOME
+          <ul>
+          </ul></li>
+        <li>CSS
+        <ul>
+        
+        </ul></li>
+        <li>SVG
+          <ul>
+          <li><a href= "midtermcle.php">CLE1: Midterm </a></li>
+        </ul></li>
+    </ul>
+    <ul>
+      <li>CANVAS
+            <ul>
+            
+            </ul></li>
+</ul>
+  </div>
+  </header>
+</body>
+</html>
